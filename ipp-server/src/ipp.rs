008cdcc15d4b1d8a81c1f6edb5f8aeaf84ed2e65
Fabian Schmitthenner <development@schmitthenner.eu>
2024-02-03 21:31:59 +0000

    refactor code related to ipp into its own file

---
+++ ipp-server/src/ipp.rs
@@ -0,0 +1,218 @@
+use bytes::{Buf, BufMut, Bytes, BytesMut};
+
+
+
+#[repr(u8)]
+#[derive(Copy, Clone, Debug)]
+pub enum DelimiterTag {
+    OperationAttributes = 0x01,
+    JobAttributes = 0x02,
+    EndOfAttributes = 0x03,
+    PrinterAttributes = 0x04,
+    UnsupportedAttributes = 0x05,
+}
+
+#[derive(Debug)] 
+#[derive(PartialEq)]
+#[repr(u8)]
+pub enum ValueTag {
+    Integer = 0x21,
+    Boolean = 0x22,
+    Enum = 0x23,
+    OctetString = 0x30,
+    DateTime = 0x31,
+    Resolution = 0x32,
+    RangeOfInteger = 0x33,
+    BegCollection = 0x34,
+    TextWithLanguage = 0x35,
+    NameWithLanguage = 0x36,
+    EndCollection = 0x37,
+    NameWithoutLanguage = 0x42,
+    Keyword = 0x44,
+    Uri = 0x45,
+    UriSchema = 0x46,
+    Charset = 0x47,
+    NaturalLanguage = 0x48,
+    MimeMediaType = 0x49,
+}
+
+
+impl DelimiterTag {
+    fn parse_tag(tag: u8) -> Option<Self> {
+        match tag {
+            0x01 => Some(DelimiterTag::OperationAttributes),
+            0x02 => Some(DelimiterTag::JobAttributes),
+            0x03 => Some(DelimiterTag::EndOfAttributes),
+            0x04 => Some(DelimiterTag::PrinterAttributes),
+            0x05 => Some(DelimiterTag::UnsupportedAttributes),
+            _ => None,
+        }
+    }
+}
+
+impl ValueTag {
+    fn parse_tag(tag: u8) -> Option<Self> {
+        match tag {
+            0x21 => Some(ValueTag::Integer),
+            0x22 => Some(ValueTag::Boolean),
+            0x23 => Some(ValueTag::Enum),
+            0x30 => Some(ValueTag::OctetString),
+            0x31 => Some(ValueTag::DateTime),
+            0x32 => Some(ValueTag::Resolution),
+            0x33 => Some(ValueTag::RangeOfInteger),
+            0x34 => Some(ValueTag::BegCollection),
+            0x35 => Some(ValueTag::TextWithLanguage),
+            0x36 => Some(ValueTag::NameWithLanguage),
+            0x37 => Some(ValueTag::EndCollection),
+            0x42 => Some(ValueTag::NameWithoutLanguage),
+            0x44 => Some(ValueTag::Keyword),
+            0x45 => Some(ValueTag::Uri),
+            0x46 => Some(ValueTag::UriSchema),
+            0x47 => Some(ValueTag::Charset),
+            0x48 => Some(ValueTag::NaturalLanguage),
+            0x49 => Some(ValueTag::MimeMediaType),
+            other => { println!("Encountered unknown tag {}", other); None },
+        }
+    }
+}
+
+#[derive(Debug)]
+pub enum AttributeValue {
+    Keyword(String),
+    KeywordList(Vec<String>),
+    Other(ValueTag, Bytes),
+    OtherList(ValueTag, Vec<Bytes>)
+}
+
+impl AttributeValue {
+    fn parse(tag: ValueTag, b: &[u8]) -> Self {
+        match tag {
+            ValueTag::Keyword => {
+                AttributeValue::Keyword(String::from(String::from_utf8_lossy(b)))
+            },
+            other => {
+                AttributeValue::Other(other, Bytes::copy_from_slice(b))
+            }
+        }
+    }
+
+    fn parse_next(self, b: &[u8]) -> Self {
+        match self {
+            AttributeValue::Keyword(s) => AttributeValue::KeywordList(Vec::from([s, String::from(String::from_utf8_lossy(b))])),
+            AttributeValue::KeywordList(mut s) => { 
+                s.push(String::from(String::from_utf8_lossy(b)));
+                AttributeValue::KeywordList(s)
+            },
+            AttributeValue::Other(tag, s) => AttributeValue::OtherList(tag, Vec::from([s, Bytes::copy_from_slice(b)])),
+            AttributeValue::OtherList(tag, mut s) => {
+                s.push(Bytes::copy_from_slice(b));
+                AttributeValue::OtherList(tag, s)
+            }
+        }
+    }
+}
+
+#[derive(Debug)]
+struct Attribute {
+    kind: DelimiterTag,
+    name: String,
+    value: AttributeValue,
+}
+
+#[derive(Debug)]
+pub struct IncomingMessage {
+    version_major: u8,
+    version_minor: u8,
+    pub operation_id: u16,
+    pub request_id: u32,
+    attributes: Vec<Attribute>
+}
+
+impl IncomingMessage {
+    pub fn get_attribute(&self, name: &str) -> Option<&AttributeValue> {
+        for attr in &self.attributes {
+            if attr.name == name {
+                return Some(&attr.value);
+            }
+        }
+        None
+    }
+}
+
+pub fn parse(b: &mut impl Buf) -> Option<IncomingMessage> {
+    let version_major = b.get_u8();
+    let version_minor = b.get_u8();
+    let operation_id = b.get_u16();
+    let request_id = b.get_u32();
+    println!("Version: {}.{} Operation: {}", version_major, version_minor, operation_id);
+    
+    let attributes = parse_attributes(b);
+    
+    Some(IncomingMessage {
+        version_major,
+        version_minor,
+        request_id,
+        operation_id,
+        attributes
+    })
+}
+
+
+fn parse_attributes(b: &mut impl Buf) -> Vec<Attribute> {
+    let mut delimiter = DelimiterTag::EndOfAttributes;
+    let mut result = Vec::new();
+    let mut current_attribute: Option<Attribute> = None;
+    loop {
+        let tag = b.get_u8();
+        match DelimiterTag::parse_tag(tag) {
+            Some(DelimiterTag::EndOfAttributes) => {
+                break
+            },
+            Some(d) => {
+                delimiter = d;
+                continue;
+            },
+            None => {}
+        }
+        
+        let tag = ValueTag::parse_tag(tag);
+        let name_length = b.get_u16();
+        let name = b.copy_to_bytes(name_length as usize);
+        let value_length = b.get_u16();
+        let value = b.copy_to_bytes(value_length as usize);
+
+        if name_length == 0 {
+            let mut new_attributes = current_attribute.unwrap();
+            new_attributes = Attribute {
+                value: new_attributes.value.parse_next(&value),
+                ..new_attributes
+            };
+            current_attribute = Some(new_attributes);
+        } else {
+            let attribute = Attribute {
+                kind: delimiter,
+                name: String::from(String::from_utf8_lossy(&name)),
+                value: AttributeValue::parse(tag.unwrap(), &value)
+            };
+            if let Some(attr) = current_attribute { result.push(attr) };
+            current_attribute = Some(attribute);
+        }
+    }
+    if let Some(attr) = current_attribute { result.push(attr) };
+    result
+}
+
+pub fn send_attribute(tpe: ValueTag, name: &str, value: &str, buf: &mut BytesMut) {
+    buf.put_u8(tpe as u8);
+
+    let name = name.as_bytes();
+    let value = value.as_bytes();
+
+    let name_length = name.len() as u16;
+    let value_length = value.len() as u16;
+
+    buf.put_u16(name_length);
+    buf.put(name);
+    buf.put_u16(value_length);
+    buf.put(value);
+}
--- ipp-server/src/main.rs
+++ ipp-server/src/main.rs
@@ -21,252 +21,37 @@ use std::time::{SystemTime, UNIX_EPOCH};
 use tokio::sync::mpsc;
 use tokio::sync::oneshot;
 
-
-#[repr(u8)]
-#[derive(Copy, Clone, Debug)]
-enum DelimiterTag {
-    OperationAttributes = 0x01,
-    JobAttributes = 0x02,
-    EndOfAttributes = 0x03,
-    PrinterAttributes = 0x04,
-    UnsupportedAttributes = 0x05,
-}
-
-#[derive(Debug)] 
-#[derive(PartialEq)]
-#[repr(u8)]
-enum ValueTag {
-    Integer = 0x21,
-    Boolean = 0x22,
-    Enum = 0x23,
-    OctetString = 0x30,
-    DateTime = 0x31,
-    Resolution = 0x32,
-    RangeOfInteger = 0x33,
-    BegCollection = 0x34,
-    TextWithLanguage = 0x35,
-    NameWithLanguage = 0x36,
-    EndCollection = 0x37,
-    NameWithoutLanguage = 0x42,
-    Keyword = 0x44,
-    Uri = 0x45,
-    UriSchema = 0x46,
-    Charset = 0x47,
-    NaturalLanguage = 0x48,
-    MimeMediaType = 0x49,
-}
+mod ipp;
 
 
-impl DelimiterTag {
-    fn parse_tag(tag: u8) -> Option<Self> {
-        match tag {
-            0x01 => Some(DelimiterTag::OperationAttributes),
-            0x02 => Some(DelimiterTag::JobAttributes),
-            0x03 => Some(DelimiterTag::EndOfAttributes),
-            0x04 => Some(DelimiterTag::PrinterAttributes),
-            0x05 => Some(DelimiterTag::UnsupportedAttributes),
-            _ => None,
-        }
-    }
-}
-
-impl ValueTag {
-    fn parse_tag(tag: u8) -> Option<Self> {
-        match tag {
-            0x21 => Some(ValueTag::Integer),
-            0x22 => Some(ValueTag::Boolean),
-            0x23 => Some(ValueTag::Enum),
-            0x30 => Some(ValueTag::OctetString),
-            0x31 => Some(ValueTag::DateTime),
-            0x32 => Some(ValueTag::Resolution),
-            0x33 => Some(ValueTag::RangeOfInteger),
-            0x34 => Some(ValueTag::BegCollection),
-            0x35 => Some(ValueTag::TextWithLanguage),
-            0x36 => Some(ValueTag::NameWithLanguage),
-            0x37 => Some(ValueTag::EndCollection),
-            0x42 => Some(ValueTag::NameWithoutLanguage),
-            0x44 => Some(ValueTag::Keyword),
-            0x45 => Some(ValueTag::Uri),
-            0x46 => Some(ValueTag::UriSchema),
-            0x47 => Some(ValueTag::Charset),
-            0x48 => Some(ValueTag::NaturalLanguage),
-            0x49 => Some(ValueTag::MimeMediaType),
-            other => { println!("Encountered unknown tag {}", other); None },
-        }
-    }
-}
-
-#[derive(Debug)]
-enum AttributeValue {
-    Keyword(String),
-    KeywordList(Vec<String>),
-    Other(ValueTag, Bytes),
-    OtherList(ValueTag, Vec<Bytes>)
-}
-
-
-impl AttributeValue {
-    fn parse(tag: ValueTag, b: &[u8]) -> Self {
-        match tag {
-            ValueTag::Keyword => {
-                AttributeValue::Keyword(String::from(String::from_utf8_lossy(b)))
-            },
-            other => {
-                AttributeValue::Other(other, Bytes::copy_from_slice(b))
-            }
-        }
-    }
-
-    fn parse_next(self, b: &[u8]) -> Self {
-        match self {
-            AttributeValue::Keyword(s) => AttributeValue::KeywordList(Vec::from([s, String::from(String::from_utf8_lossy(b))])),
-            AttributeValue::KeywordList(mut s) => { 
-                s.push(String::from(String::from_utf8_lossy(b)));
-                AttributeValue::KeywordList(s)
-            },
-            AttributeValue::Other(tag, s) => AttributeValue::OtherList(tag, Vec::from([s, Bytes::copy_from_slice(b)])),
-            AttributeValue::OtherList(tag, mut s) => {
-                s.push(Bytes::copy_from_slice(b));
-                AttributeValue::OtherList(tag, s)
-            }
-        }
-    }
-}
-
-#[derive(Debug)]
-struct Attribute {
-    kind: DelimiterTag,
-    name: String,
-    value: AttributeValue,
-}
-
-#[derive(Debug)]
-struct IppIncomingMessage {
-    version_major: u8,
-    version_minor: u8,
-    operation_id: u16,
-    request_id: u32,
-    attributes: Vec<Attribute>
-}
-
-fn parse(b: &mut impl Buf) -> Option<IppIncomingMessage> {
-    let version_major = b.get_u8();
-    let version_minor = b.get_u8();
-    let operation_id = b.get_u16();
-    let request_id = b.get_u32();
-    println!("Version: {}.{} Operation: {}", version_major, version_minor, operation_id);
-    
-    let attributes = parse_attributes(b);
-
-    let data_size = b.remaining();
-    println!("Data size: {}", data_size);
-    
-    if data_size > 0 {
-        let job_name = attributes.iter().find(|a| a.name == "job-name").map(|a| match &a.value {
-            AttributeValue::Other(ValueTag::NameWithoutLanguage, b) => {
-                String::from(String::from_utf8_lossy(&b))
-            },
-            _ => String::from("PRINTED_UNKNOWN_NAME")
-        }).unwrap_or(String::from("PRINTED_UNKNOWN_NAME"));
-        store_pdf(b.copy_to_bytes(data_size), job_name.as_str());
-    }
-    
-    Some(IppIncomingMessage {
-        version_major,
-        version_minor,
-        request_id,
-        operation_id,
-        attributes
-    })
-}
-
-
-fn parse_attributes(b: &mut impl Buf) -> Vec<Attribute> {
-    let mut delimiter = DelimiterTag::EndOfAttributes;
-    let mut result = Vec::new();
-    let mut current_attribute: Option<Attribute> = None;
-    loop {
-        let tag = b.get_u8();
-        match DelimiterTag::parse_tag(tag) {
-            Some(DelimiterTag::EndOfAttributes) => {
-                break
-            },
-            Some(d) => {
-                delimiter = d;
-                continue;
-            },
-            None => {}
-        }
-        
-        let tag = ValueTag::parse_tag(tag);
-        let name_length = b.get_u16();
-        let name = b.copy_to_bytes(name_length as usize);
-        let value_length = b.get_u16();
-        let value = b.copy_to_bytes(value_length as usize);
-
-        if name_length == 0 {
-            let mut new_attributes = current_attribute.unwrap();
-            new_attributes = Attribute {
-                value: new_attributes.value.parse_next(&value),
-                ..new_attributes
-            };
-            current_attribute = Some(new_attributes);
-        } else {
-            let attribute = Attribute {
-                kind: delimiter,
-                name: String::from(String::from_utf8_lossy(&name)),
-                value: AttributeValue::parse(tag.unwrap(), &value)
-            };
-            if let Some(attr) = current_attribute { result.push(attr) };
-            current_attribute = Some(attribute);
-        }
-    }
-    if let Some(attr) = current_attribute { result.push(attr) };
-    result
-}
-
-fn send_attribute(tpe: ValueTag, name: &str, value: &str, buf: &mut BytesMut) {
-    buf.put_u8(tpe as u8);
-
-    let name = name.as_bytes();
-    let value = value.as_bytes();
-
-    let name_length = name.len() as u16;
-    let value_length = value.len() as u16;
-
-    buf.put_u16(name_length);
-    buf.put(name);
-    buf.put_u16(value_length);
-    buf.put(value);
-}
 
 fn send_document_type(buf: &mut BytesMut) {
-    buf.put_u8(DelimiterTag::OperationAttributes as u8);
+    buf.put_u8(ipp::DelimiterTag::OperationAttributes as u8);
     
-    send_attribute(ValueTag::Charset, "attributes-charset", "utf-8", buf);
-    send_attribute(ValueTag::NaturalLanguage, "attributes-natural-language", "en-us", buf);
+    ipp::send_attribute(ipp::ValueTag::Charset, "attributes-charset", "utf-8", buf);
+    ipp::send_attribute(ipp::ValueTag::NaturalLanguage, "attributes-natural-language", "en-us", buf);
 
-    buf.put_u8(DelimiterTag::PrinterAttributes as u8);    
-    send_attribute(ValueTag::MimeMediaType, "document-format-supported", "application/pdf", buf);
-    send_attribute(ValueTag::MimeMediaType, "", "application/postscript", buf);
+    buf.put_u8(ipp::DelimiterTag::PrinterAttributes as u8);    
+    ipp::send_attribute(ipp::ValueTag::MimeMediaType, "document-format-supported", "application/pdf", buf);
+    ipp::send_attribute(ipp::ValueTag::MimeMediaType, "", "application/postscript", buf);
 
-    send_attribute(ValueTag::Keyword, "ipp-versions-supported", "1.1", buf);
+    ipp::send_attribute(ipp::ValueTag::Keyword, "ipp-versions-supported", "1.1", buf);
     
-    send_attribute(ValueTag::MimeMediaType, "document-format-default", "application/pdf", buf);
-    send_attribute(ValueTag::Keyword, "compression-supported", "none", buf);
-    send_attribute(ValueTag::Boolean, "printer-is-accepting-jobs", "\x01", buf);
-    send_attribute(ValueTag::Enum, "printer-state", "\x00\x00\x00\x03", buf);
-    send_attribute(ValueTag::Keyword, "printer-state-reasons", "none", buf);
+    ipp::send_attribute(ipp::ValueTag::MimeMediaType, "document-format-default", "application/pdf", buf);
+    ipp::send_attribute(ipp::ValueTag::Keyword, "compression-supported", "none", buf);
+    ipp::send_attribute(ipp::ValueTag::Boolean, "printer-is-accepting-jobs", "\x01", buf);
+    ipp::send_attribute(ipp::ValueTag::Enum, "printer-state", "\x00\x00\x00\x03", buf);
+    ipp::send_attribute(ipp::ValueTag::Keyword, "printer-state-reasons", "none", buf);
     
-    send_attribute(ValueTag::Uri, "printer-uri-supported", "ipp://192.168.0.10/remarkable-printer", buf);
-    send_attribute(ValueTag::NameWithoutLanguage, "printer-name", "remarkable-test-printer", buf);
-    send_attribute(ValueTag::Keyword, "uri-authentication-supported", "none", buf);
-    send_attribute(ValueTag::Keyword, "uri-security-supported", "none", buf);
+    ipp::send_attribute(ipp::ValueTag::Uri, "printer-uri-supported", "ipp://192.168.0.10/remarkable-printer", buf);
+    ipp::send_attribute(ipp::ValueTag::NameWithoutLanguage, "printer-name", "remarkable-test-printer", buf);
+    ipp::send_attribute(ipp::ValueTag::Keyword, "uri-authentication-supported", "none", buf);
+    ipp::send_attribute(ipp::ValueTag::Keyword, "uri-security-supported", "none", buf);
     
-    send_attribute(ValueTag::Enum, "operations-supported", "\x00\x00\x00\x02", buf);
-    send_attribute(ValueTag::Enum, "", "\x00\x00\x00\x04", buf);
-    send_attribute(ValueTag::Enum, "", "\x00\x00\x00\x0a", buf);
-    send_attribute(ValueTag::Enum, "", "\x00\x00\x00\x0b", buf);
+    ipp::send_attribute(ipp::ValueTag::Enum, "operations-supported", "\x00\x00\x00\x02", buf);
+    ipp::send_attribute(ipp::ValueTag::Enum, "", "\x00\x00\x00\x04", buf);
+    ipp::send_attribute(ipp::ValueTag::Enum, "", "\x00\x00\x00\x0a", buf);
+    ipp::send_attribute(ipp::ValueTag::Enum, "", "\x00\x00\x00\x0b", buf);
     
 }
 
@@ -321,19 +106,19 @@ fn store_pdf(bytes: Bytes, job_name: &str) {
 async fn print_job(buf: &mut BytesMut, state: JobHandler) {
     let job_id = state.add_job().await;
     
-    buf.put_u8(DelimiterTag::OperationAttributes as u8);
+    buf.put_u8(ipp::DelimiterTag::OperationAttributes as u8);
     
-    send_attribute(ValueTag::Charset, "attributes-charset", "utf-8", buf);
-    send_attribute(ValueTag::NaturalLanguage, "attributes-natural-language", "en-us", buf);
+    ipp::send_attribute(ipp::ValueTag::Charset, "attributes-charset", "utf-8", buf);
+    ipp::send_attribute(ipp::ValueTag::NaturalLanguage, "attributes-natural-language", "en-us", buf);
 
     write_job(job_id, buf);
 }
 
 fn validate_job(buf: &mut BytesMut) {
-    buf.put_u8(DelimiterTag::OperationAttributes as u8);
+    buf.put_u8(ipp::DelimiterTag::OperationAttributes as u8);
     
-    send_attribute(ValueTag::Charset, "attributes-charset", "utf-8", buf);
-    send_attribute(ValueTag::NaturalLanguage, "attributes-natural-language", "en-us", buf);
+    ipp::send_attribute(ipp::ValueTag::Charset, "attributes-charset", "utf-8", buf);
+    ipp::send_attribute(ipp::ValueTag::NaturalLanguage, "attributes-natural-language", "en-us", buf);
 }
 
 
@@ -341,7 +126,7 @@ fn validate_job(buf: &mut BytesMut) {
 async fn send_jobs(buf: &mut BytesMut, state: JobHandler) {
     let jobs = state.fetch_jobs().await;
 
-    buf.put_u8(DelimiterTag::OperationAttributes as u8);
+    buf.put_u8(ipp::DelimiterTag::OperationAttributes as u8);
     
 
     println!("Jobs: {:?}", &jobs);
@@ -354,7 +139,7 @@ async fn send_jobs(buf: &mut BytesMut, state: JobHandler) {
 async fn send_job(buf: &mut BytesMut, job_id: u32, state: JobHandler) {
     let jobs = state.fetch_jobs().await;
 
-    buf.put_u8(DelimiterTag::OperationAttributes as u8);
+    buf.put_u8(ipp::DelimiterTag::OperationAttributes as u8);
     
 
     println!("Jobs: {:?}", &jobs);
@@ -372,18 +157,18 @@ fn write_job(job_id: u32, buf: &mut BytesMut) {
     let mut job_uri = "ipp://192.168.0.10/remarkable-printer/jobs/".to_string();
     job_uri.push_str(job_id.to_string().as_str());
 
-    buf.put_u8(DelimiterTag::JobAttributes as u8);
-    send_attribute(ValueTag::Charset, "attributes-charset", "utf-8", buf);
-    send_attribute(ValueTag::NaturalLanguage, "attributes-natural-language", "en-us", buf);
-    send_attribute(ValueTag::Integer, "job-id", job_id.as_str(), buf);
+    buf.put_u8(ipp::DelimiterTag::JobAttributes as u8);
+    ipp::send_attribute(ipp::ValueTag::Charset, "attributes-charset", "utf-8", buf);
+    ipp::send_attribute(ipp::ValueTag::NaturalLanguage, "attributes-natural-language", "en-us", buf);
+    ipp::send_attribute(ipp::ValueTag::Integer, "job-id", job_id.as_str(), buf);
     
-    send_attribute(ValueTag::Uri, "job-uri", job_uri.as_str(), buf);
+    ipp::send_attribute(ipp::ValueTag::Uri, "job-uri", job_uri.as_str(), buf);
     
 
-    send_attribute(ValueTag::Enum, "job-state", "\x00\x00\x00\x09", buf);
-    send_attribute(ValueTag::Keyword, "job-state-reasons", "job-completed-successfully", buf);
-    send_attribute(ValueTag::NameWithoutLanguage, "job-name", "com.google.android.apps.photos.Image", buf);
-    send_attribute(ValueTag::Uri, "printer-uri", "ipp://192.168.0.10/remarkable-printer", buf);                
+    ipp::send_attribute(ipp::ValueTag::Enum, "job-state", "\x00\x00\x00\x09", buf);
+    ipp::send_attribute(ipp::ValueTag::Keyword, "job-state-reasons", "job-completed-successfully", buf);
+    ipp::send_attribute(ipp::ValueTag::NameWithoutLanguage, "job-name", "com.google.android.apps.photos.Image", buf);
+    ipp::send_attribute(ipp::ValueTag::Uri, "printer-uri", "ipp://192.168.0.10/remarkable-printer", buf);                
 }
 
 async fn response(status_code: u16, request_id: u32, request_type: u16, job_id: u32, state: JobHandler) -> Bytes {
@@ -408,7 +193,7 @@ async fn response(status_code: u16, request_id: u32, request_type: u16, job_id: 
         panic!("Unknown request type: {}", request_type)
     }
 
-    buf.put_u8(DelimiterTag::EndOfAttributes as u8);
+    buf.put_u8(ipp::DelimiterTag::EndOfAttributes as u8);
 
     buf.freeze()
 }
@@ -424,10 +209,23 @@ async fn reply(state: JobHandler, request: Request<hyper::body::Incoming>) -> Re
     
     let mut a = request.collect().await.unwrap().aggregate();
 
-    let message = parse(&mut a).unwrap();
+    let message = ipp::parse(&mut a).unwrap();
 
-    let job_id = message.attributes.iter().find(|a| a.name == "job-id").map(|a| match &a.value {
-        AttributeValue::Other(ValueTag::Integer, b) => {
+    let data_size = a.remaining();
+    println!("Data size: {}", data_size);
+    
+    if data_size > 0 {
+        let job_name = message.get_attribute("job-name").map(|a| match a {
+            ipp::AttributeValue::Other(ipp::ValueTag::NameWithoutLanguage, b) => {
+                String::from(String::from_utf8_lossy(&b))
+            },
+            _ => String::from("PRINTED_UNKNOWN_NAME")
+        }).unwrap_or(String::from("PRINTED_UNKNOWN_NAME"));
+        store_pdf(a.copy_to_bytes(data_size), job_name.as_str());
+    }
+    
+    let job_id = message.get_attribute("job-id").map(|a| match a {
+        ipp::AttributeValue::Other(ipp::ValueTag::Integer, b) => {
             b.clone().get_u32()
         },
         _ => 0
